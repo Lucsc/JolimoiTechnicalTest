670d85c9abb93b90d3851856cf394b371322a7fd
lschmitt <lschmitt@vpwhite.com>
2025-08-12 14:30:39 +0200

    [ADD] Loading handling + remove unneccessary call

--- Project/frontend/src/App.jsx
+++ Project/frontend/src/App.jsx
@@ -5,7 +5,15 @@ function App() {
     const [number, setNumber] = useState("");
     const [result, setResult] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
 
+    const handleErrorInvalid = (e) => {
+        e.preventDefault();
+        const nb = Number(number);
+        if (isNaN(nb) || nb < 0 || nb > 100)
+            setError("Number is invalid. Please enter a number between 0 and 100.");
+    };
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
         setResult("");
@@ -20,8 +28,10 @@ function App() {
         
         if (window.currentSSE) {
             window.currentSSE.close();
+            window.currentSSE = null;
         }
         const evtSource = new EventSource(`http://localhost:3000/convertToRoman/${number}`);
+        setLoading(true);
         window.currentSSE = evtSource;
         
         evtSource.onmessage = (event) => {
@@ -33,31 +43,38 @@ function App() {
                 setResult(data.result);
                 evtSource.close();
             }
+            setLoading(false);
         }
         
         evtSource.onerror = (e) => {
             console.error("Error occurred in SSE:", e);
             setError("An error occurred while fetching the result. Please try again.");
             evtSource.close();
+            setLoading(false);
         }
         
     };
 
     return (
         <div style={{ padding: "2rem", alignItems: "center", border: "1px solid black", width : "30%", margin: "auto", borderRadius: "10px", textAlign: "center", height : "100%"}}>
-            <h1>Convert literal numbers to roman numbers</h1>
+            <h1>Convert literal numbers between 0 and 100 to roman numbers</h1>
             <form onSubmit={handleSubmit}>
                 <input
+                    required
                     type="number"
                     min="0"
                     max="100"
                     value={number}
                     onChange={(e) => setNumber(e.target.value)}
-                    onInvalid={handleSubmit}
+                    onInvalid={handleErrorInvalid}
+                    onError={handleErrorInvalid}
+                    title="Please enter a number between 0 and 100"
                 />
                 <button type="submit">Convert</button>
             </form>
             {result && <h2>Result : {result}</h2>}
+            {error && <h2 style={{ color: "red" }}>Error: {error}</h2>}
+            {loading && <h2>Loading...</h2>}
         </div>
     );
 }
