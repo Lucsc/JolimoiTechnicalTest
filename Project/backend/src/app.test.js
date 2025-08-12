2296e488f890a67d892c3941049e2d45bbf63fa1
lschmitt <lschmitt@vpwhite.com>
2025-08-12 15:19:39 +0200

    [ADD] test with string

--- Project/backend/src/app.test.js
+++ Project/backend/src/app.test.js
@@ -29,4 +29,12 @@ describe('GET /convertToRoman/:number', () => {
         // On récupère le corps sous forme de texte (supertest ne parse pas SSE), on vérifie la chaîne
         expect(res.text).toContain('Number invalid, should be between 0 and 100');
     });
+    
+    test('send invalid response for not number', async () => {
+        const res = await request(app).get('/convertToRoman/abc');
+        expect(res.statusCode).toBe(200);
+        expect(res.header['content-type']).toMatch(/text\/event-stream/);
+        // On récupère le corps sous forme de texte (supertest ne parse pas SSE), on vérifie la chaîne
+        expect(res.text).toContain('Number invalid, should be between 0 and 100');
+    });
 });
